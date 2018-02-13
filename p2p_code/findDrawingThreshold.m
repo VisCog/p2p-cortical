63557c0359470ff820e077f4e4d5405348fa3eff
Ione Fine <ionefine@uw.edu>
2018-02-12 19:45:12 -0800

    fixed various
    -

--- p2p_code/findDrawingThreshold.m
+++ p2p_code/findDrawingThreshold.m
@@ -2,8 +2,8 @@ function err = findDrawingThreshold(p, sim_phosphene, respfac, real_area)
 
 % finds the threshold that minimizes difference between simulated and real area drawings across all conditions
 for cc = 1: length(respfac)
-    sim_area(1,cc) = length( find(sim_phosphene * respfac(cc)>p.thr) );
+    sim_area(1,cc) = sum((sim_phosphene * respfac(cc)>p.thr));
 end
 
-err = sum(sqrt(((log10(sim_area)-log10(real_area)).^2)));
+err = sum(sqrt(((sim_area-real_area).^2)));
 end
--- p2p_code/p2p_ebs.m
+++ p2p_code/p2p_ebs.m
@@ -7,7 +7,7 @@ function D=p2p_ebs()
 %   http://dx.doi.org/10.1016/j.neuron.2016.11.008
 close all
 
-sites = 1;
+sites = 1:5;
 
 Dreal = getData(sites);    % Data per trials
 opts = getPlotOpts();  % Plot options
@@ -17,18 +17,21 @@ Dreal = get_tsform(Dreal);
 
 c = getcorticalparams();
 
-sim=simulateData(Dreal, c); % generate simulated data to match the Winawer
+sim = simulateData(Dreal, c); % generate simulated data to match the Winawer
 
-if 1
-opts.titlestr = 'winawer (4a)';
+figure_RealVSPred(Dreal, sim, opts)
+
+opts.titlestr = 'Winawer (4a)';
 opts.marker = 'o';
 opts.color = 'b';
+opts.line = 0;
 figure_CvsA(Dreal, opts, 'subplot(3, 1, 2)');
 
 Dsim=Dreal; % replace with electrical field prediction
 opts.titlestr = 'EF phosphene (4a)';
 opts.marker = 'o';
 opts.color = 'r';
+opts.line = 1;
 for ii = sites
     Dsim(ii).poly_area.val = sim(ii).EF.poly_area.val;
 end
@@ -37,11 +40,14 @@ figure_CvsA(Dsim,  opts, 'subplot(3, 1, 2)');
 opts.titlestr = 'CM phosphene (4a)'; % include receptive fields
 opts.marker = 'o';
 opts.color = 'g';
+opts.line = 1; 
 for ii = sites
     Dsim(ii).poly_area.val = sim(ii).CM.poly_area.val;
 end
 figure_CvsA(Dsim, opts, 'subplot(3, 1, 2)');
- end
+
+
+
 
 % return
 % % Figure 4b
@@ -118,19 +124,18 @@ plotcortgrid(64 * (c.orientationMap+pi)/(pi*2), c, 'Orientation pinwheels', hsv(
 plotcortgrid(64 * c.odMap, c, 'Ocular dominance columns', gray(64), 2, 'subplot(1, 1, 1)');
 plotcortgrid(64*c.sizeMap/6, c, 'Receptive field size', hot(64), 3, 'subplot(1, 1, 1)');
 
-  if c.smallflag
-            Dsim(1).EF.drawingthreshold =  41.4854;
-            Dsim(1).CM.drawingthreshold = 41.3328;
-            Dsim(2).EF.drawingthreshold = 73.1133;
-            Dsim(2).CM.drawingthreshold = 80.668;
-            Dsim(3).EF.drawingthreshold = 24.7656;
-            Dsim(3).CM.drawingthreshold = 35.6641;
-            Dsim(4).EF.drawingthreshold = 40.875;
-            Dsim(4).CM.drawingthreshold = 41.5313;
-            Dsim(5).EF.drawingthreshold = 224.25;
-            Dsim(5).CM.drawingthreshold = 239;
-            
-  end
+if c.smallflag
+    Dsim(1).EF.drawingthreshold =  41.4854;
+    Dsim(1).CM.drawingthreshold = 41.3328;
+    Dsim(2).EF.drawingthreshold = 73.1133;
+    Dsim(2).CM.drawingthreshold = 80.668;
+    Dsim(3).EF.drawingthreshold = 24.7656;
+    Dsim(3).CM.drawingthreshold = 35.6641;
+    Dsim(4).EF.drawingthreshold = 40.875;
+    Dsim(4).CM.drawingthreshold = 41.5313;
+    Dsim(5).EF.drawingthreshold = 224.25;
+    Dsim(5).CM.drawingthreshold = 239; 
+end
         
 % set up the temporal model for integrating current over time
 tp = gettemporalparams();
@@ -156,7 +161,7 @@ for ii=1:length(D) % for each site
         
         for pixNum = 1:length(c.x(:))
             if mod(pixNum, 1000)==0
-           %     disp([num2str(round((100*pixNum)/length(c.x(:)))),  '% complete' ]);
+                disp([num2str(round((100*pixNum)/length(c.x(:)))),  '% complete' ]);
             end
             if Dsim(ii).EF.cortex(pixNum) > 0.25
                 
@@ -191,31 +196,31 @@ for ii=1:length(D) % for each site
         for cc=1:length(D(ii).condition.val)
             Dsim(ii).CI.val(cc) = 100 * max(p2p_finite_element(tp, D(ii).tsform(cc).val)); % the scaling due to current integration over time
         end
+  
+        idx=find(D(ii).stimulation_data.val>0);
+        if ii==5; idx=setdiff(idx,3:5); end % drawings not obtained
         
-        
-        gvals=find(D(ii).stimulation_data.val>0);
-        if ii==5; gvals=setdiff(gvals,3:5); end % drawings not obtained
-        poly_area = D(ii).poly_area.val(gvals);
+        poly_area = D(ii).poly_area.val(idx);
         poly_area(isnan(poly_area))=0;
             
         if ~c.smallflag
-            p.thr = 5;  p=UWfit('findDrawingThreshold', p, {'thr'}, Dsim(ii).EF.normalizedphosphene(:), Dsim(ii).CI.val(gvals), ...
+            p.thr = 50;  p=UWfit('findDrawingThreshold', p, {'thr'}, Dsim(ii).EF.normalizedphosphene(:), Dsim(ii).CI.val(idx), ...
                 (poly_area * c.pixperdeg^2));
             Dsim(ii).EF.drawingthreshold = p.thr;
             disp(['EF drawing threshold = ', num2str(Dsim(ii).EF.drawingthreshold)]);
             
-            p.thr = 5; p=UWfit('findDrawingThreshold', p, {'thr'}, Dsim(ii).CM.normalizedphosphene(:), Dsim(ii).CI.val(gvals), ...
+            p.thr = 50; p=UWfit('findDrawingThreshold', p, {'thr'}, Dsim(ii).CM.normalizedphosphene(:), Dsim(ii).CI.val(idx), ...
                 (poly_area * c.pixperdeg^2));
             Dsim(ii).CM.drawingthreshold = p.thr;
             disp(['CM drawing threshold = ', num2str(Dsim(ii).CM.drawingthreshold)]);
         end
-        for cc=gvals
-            Dsim(ii).EF.poly_area.val(cc) = (1/(c.pixperdeg^2)) * ...
-                length( find((Dsim(ii).EF.normalizedphosphene(:) * ...
-                Dsim(ii).CI.val(cc) )>  Dsim(ii).EF.drawingthreshold) );
-            Dsim(ii).CM.poly_area.val(cc) = (1/(c.pixperdeg^2)) * ...
-                length( find((Dsim(ii).CM.normalizedphosphene(:) * ...
-                Dsim(ii).CI.val(cc) )>  Dsim(ii).CM.drawingthreshold) );
+        for cc=1:length(idx)
+            Dsim(ii).EF.poly_area.val(idx(cc)) = (1/(c.pixperdeg^2)) * ...
+                length( find((Dsim(ii).EF.normalizedphosphene(:) * Dsim(ii).CI.val(idx(cc)) ) ...
+                >  Dsim(ii).EF.drawingthreshold) );
+            Dsim(ii).CM.poly_area.val(idx(cc)) = (1/(c.pixperdeg^2)) * ...
+                length( find((Dsim(ii).CM.normalizedphosphene(:) *  Dsim(ii).CI.val(idx(cc)) ) ...
+                >  Dsim(ii).CM.drawingthreshold) );
         end
         
     end
@@ -225,7 +230,8 @@ end % site
 
 function c = getcorticalparams()
 
-c.smallflag = 1;
+c.smallflag = 0;
+if c.smallflag disp('simulating small electrodes!'); end
 
 if c.smallflag
     c.e_size=[50/1000; 50/1000;  50/1000;  50/1000;  50/1000];
@@ -260,8 +266,7 @@ c.gridColor = [1,1,0];
 c.cortexSize = [80,100];  % %[height, width] Size of cortical maps (mm)
 c.cortexCenter = [30,0]; % center of electrode array (mm on cortex)
 
-
-c2r = 5; % each degree is represented by about ~280 microns in the retina
+c2r = 4; % each degree is represented by about ~280 microns in the retina
 c.retinaSize = [70,70]; %  [height, width diameter in degrees]
 c.retinaCenter = [0,0];
 
@@ -278,7 +283,6 @@ c.n = 201;
 disp('generating visual/cortical template');
 % define cortex meshgrid
 
-
 c.xax = linspace(.5/c.pixpermm,c.cortexSize(2)-.5/c.pixpermm,c.cortexSize(2)*c.pixpermm) + c.cortexCenter(1) - c.cortexSize(2)/2;
 c.yax = linspace(.5/c.pixpermm,c.cortexSize(1)-.5/c.pixpermm,c.cortexSize(1)*c.pixpermm) + c.cortexCenter(2) - c.cortexSize(1)/2;
 [c.x,c.y] = meshgrid(c.xax,c.yax);
@@ -287,7 +291,6 @@ c.yax = linspace(.5/c.pixpermm,c.cortexSize(1)-.5/c.pixpermm,c.cortexSize(1)*c.p
 [c.orientationMap,c.odMap] = makePinwheelODMaps(c.x,c.y, 0.863);
 vr = mapinv(c, c.x+sqrt(-1)*c.y);
 c.sizeMap = max((.1652 .* abs(vr)), 0.9);
-
 
 % Define the region for cropping maps to the ranges of c.angList and c.radList
 % cropPix is used in the function 'plotcortgrid' to set the cropped pixels
@@ -500,13 +503,22 @@ opts.area = 'area';
 opts.leg_txt = cellstr(num2str(opts.sites'));
 
 % Axis scale and range
-opts.yscale = 'log';
-opts.xscale = 'log'; % 'linear'
-opts.yl     = 10.^[-1 3]; % 'linear'
+opts.yscale = 'linear';
+opts.xscale = 'linear'; % 'linear' or log
 
 switch opts.xscale
     case 'log', opts.xl = 10.^[1.5 4.5];
-    case 'linear', opts.xl = [0 1.2e4];
+    case 'linear'
+        opts.xlim = [0 300];
+        opts.xtick = 0:100:300;
+end
+
+switch opts.yscale
+    case 'log'
+        opts.yl     = 10.^[-1 3]; % 'linear'
+    case 'linear'     
+        opts.ylim = [0 500];
+        opts.ytick = 0:100:500;
 end
 
 end
@@ -559,46 +571,72 @@ end
 
 function fH = figure_CvsA(D, opts, spstr)
 
-x_all = [];
-for ii = 1:length(D)
-    if ~isempty(D(ii).condition)
-        idx = isfinite(D(ii).which_drawing.val);
-        x_all = [x_all D(ii).stimulation_data.val(idx)];
-    end
-end
-
-x_all = sort(unique(x_all));
-
 for ii = 1:length(D)
     if ~isempty(D(ii).condition)
         fH = figure(10+ii); eval(spstr)
         set(fH, 'Color', 'w' ,'name', ['site ', num2str(ii), opts.titlestr]);
         set(gca, 'FontSize', 8); hold on
         
-        fit_type = 'power';
-        idx = isfinite(D(ii).which_drawing.val);
-        switch fit_type
-            case 'linear'
-                b = regress(D(ii).poly_area.val(idx)', ...
-                    D(ii).stimulation_data.val(idx)');
-                pred_y = x_all * b;
-            case 'power'
-                [f, gof] = fit(D(ii).stimulation_data.val(idx)', ...
-                    D(ii).poly_area.val(idx)','b*x^m', ...
-                    'StartPoint',[mean(D(ii).poly_area.val(idx)) ...
-                    / mean(D(ii).stimulation_data.val(idx)) 1]);
-                pred_y = f(x_all);
-                disp(f)
-                disp(gof)
-        end
-        plot(D(ii).stimulation_data.val(idx), D(ii).poly_area.val(idx),...
-            'k', 'Marker', opts.marker, 'MarkerFaceColor', opts.color, 'MarkerSize', 8, 'LineStyle', 'none'); hold on
-        plot(x_all,  pred_y, 'Color', opts.color, 'LineWidth', 1);
-        set(gca, 'YScale', opts.yscale, 'XScale', opts.xscale, 'XTick', 10.^[0 1 2 3], 'XLim', 10.^[0 3])
-        if strcmp(opts.yscale, 'log'), set(gca, 'YLim', 10.^[-3 3]); end
+        idx=find(D(ii).stimulation_data.val>0);
+        if ii==5; idx=setdiff(idx,3:5); end % drawings not obtained    
+ 
+        D(ii).poly_area.val(isnan(D(ii).poly_area.val))=0; % replace NaN's with Zeros, since some were genuine stimulation
+      
+        % idx = isfinite(D(ii).which_drawing.val); Winawer method, excludes
+        % stimulations that didn't result in percepts
+        x_all =  D(ii).stimulation_data.val(idx);
+        y_all = D(ii).poly_area.val(idx);
+        [~, idx2]=sort(x_all);
         
+        plot(x_all, y_all ,...
+            'k', 'Marker', opts.marker, 'MarkerFaceColor', opts.color, 'MarkerSize', 8, 'LineStyle', 'none'); hold on
+        if opts.line
+            
+            plot(x_all(idx2),  y_all(idx2), 'Color', opts.color, 'LineWidth', 1);
+        end
+        set(gca, 'XLim', [ 0 ceil(1.1 * max(x_all))]);
+        set(gca,'YLim', [-.1 ceil(1.1 * max(y_all))])
+         
         xlabel('Charge Deposited per Trial (µC)')
         ylabel('Phosphene size (deg^2)')
+    end
+end
+end
+
+function fH = figure_RealVSPred(Dreal, Dsim, opts)
+
+for ii = 1:length(Dreal)
+    if ~isempty(Dreal(ii).condition)
+        fH = figure(30+ii); 
+        set(fH, 'Color', 'w' ,'name', ['site ', num2str(ii)]);
+        set(gca, 'FontSize', 8); hold on
+        
+        idx=find(Dreal(ii).stimulation_data.val>0);
+        if ii==5; idx=setdiff(idx,3:5); end % drawings not obtained    
+ 
+        Dreal(ii).poly_area.val(isnan(Dreal(ii).poly_area.val))=0; % replace NaN's with Zeros, since some were genuine stimulation
+     
+        x_all = Dreal(ii).poly_area.val(idx);
+        y_allEF = Dsim(ii).EF.poly_area.val(idx);
+        y_allCM = Dsim(ii).CM.poly_area.val(idx);
+        
+        subplot(1,2,1)
+        opts.color = 'r'; opts.marker = 'o';
+        plot(x_all, y_allEF ,...
+            'k', 'Marker', opts.marker, 'MarkerFaceColor', opts.color, 'MarkerSize', 8, 'LineStyle', 'none'); hold on
+        plot([ 0 ceil(1.1 * max(x_all))],[ 0 ceil(1.1 * max(x_all))], 'k-');
+          set(gca, 'XLim', [ 0 ceil(1.1 * max(x_all))]);
+        set(gca,'YLim', [-.1 ceil(1.1 * max([y_allEF y_allCM]))]);
+        xlabel('Real area)'); ylabel('Simulated area');
+        
+        subplot(1,2,2)
+        opts.color = 'g';
+        plot(x_all, y_allCM ,...
+            'k', 'Marker', opts.marker, 'MarkerFaceColor', opts.color, 'MarkerSize', 8, 'LineStyle', 'none'); hold on
+           plot([ 0 ceil(1.1 * max(x_all))],[ 0 ceil(1.1 * max(x_all))], 'k-'); 
+        set(gca, 'XLim', [ 0 ceil(1.1 * max(x_all))]);
+        set(gca,'YLim', [-.1 ceil(1.1 * max([y_allEF y_allCM]))]);
+        xlabel('Real area)'); ylabel('Simulated area')
     end
 end
 end
